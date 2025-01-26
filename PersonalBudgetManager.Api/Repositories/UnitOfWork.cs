758dfd7c42c6eda6465c05acc5b817a9e6a5efea
Edgar Mendez <edgar.mdz@hotmail.com>
2025-01-26 13:22:30 -0600

    Refactor UnitOfWork class to use a generic repository pattern and improve resource management

--- PersonalBudgetManager.Api/Repositories/UnitOfWork.cs
+++ PersonalBudgetManager.Api/Repositories/UnitOfWork.cs
@@ -1,22 +1,33 @@
 using PersonalBudgetManager.Api.DataContext;
-using PersonalBudgetManager.Api.DataContext.Entities;
 using PersonalBudgetManager.Api.Repositories.Interfaces;
 
 namespace PersonalBudgetManager.Api.Repositories
 {
-    public class UnitOfWork(
-        AppDbContext appDbContext,
-        Repository<User> userRepo,
-        Repository<Category> categoryRepo,
-        Repository<Income> incomeRepo,
-        Repository<Expense> expenseRepo
-    ) : IUnitOfWork
+    public class UnitOfWork(AppDbContext appDbContext) : IUnitOfWork, IDisposable
     {
         private readonly AppDbContext _appDbContext = appDbContext;
-        public Repository<User> UserRepository { get; } = userRepo;
-        public Repository<Category> CategoryRepository { get; } = categoryRepo;
-        public Repository<Income> IncomeRepository { get; } = incomeRepo;
-        public Repository<Expense> ExpenceRepository { get; } = expenseRepo;
+        private readonly Dictionary<Type, object> _repositories = [];
+
+        public void Dispose()
+        {
+            _appDbContext.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public Repository<T> GetRepository<T>()
+            where T : class, IEntity
+        {
+            var repoType = typeof(T);
+
+            if (_repositories.TryGetValue(repoType, out object? value))
+                return (Repository<T>)value;
+            else
+            {
+                Repository<T> repo = new(_appDbContext);
+                _repositories.Add(repoType, repo);
+                return repo;
+            }
+        }
 
         public async Task<int> SaveChangesAsync(CancellationToken token) =>
             await _appDbContext.SaveChangesAsync(token);
