ad5686f6652b9767c78d9e8793df59e9e8907795
Edgar Mendez <edgar.mdz@hotmail.com>
2025-01-26 21:13:17 -0600

    Add generic repository retrieval method to IUnitOfWork interface

--- PersonalBudgetManager.Api/Repositories/Interfaces/IUnitOfWork.cs
+++ PersonalBudgetManager.Api/Repositories/Interfaces/IUnitOfWork.cs
@@ -1,7 +1,11 @@
+using PersonalBudgetManager.Api.DataContext;
+
 namespace PersonalBudgetManager.Api.Repositories.Interfaces
 {
     public interface IUnitOfWork
     {
+        public Repository<T> GetRepository<T>()
+            where T : class, IEntity;
         public Task<int> SaveChangesAsync(CancellationToken token);
         public Task BeginTransactionAsync(CancellationToken token);
         public Task CommitTransactionAsync(CancellationToken token);
