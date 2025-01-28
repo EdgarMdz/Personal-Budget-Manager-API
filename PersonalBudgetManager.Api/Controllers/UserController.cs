862c6c39d508deef0116e4a6ac7c19f3a0d7d2d3
Edgar Mendez <edgar.mdz@hotmail.com>
2025-01-27 20:16:57 -0600

    Inject IUnitOfWork into UserController and initialize User repository

--- PersonalBudgetManager.Api/Controllers/UserController.cs
+++ PersonalBudgetManager.Api/Controllers/UserController.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using PersonalBudgetManager.Api.DataContext.Entities;
+using PersonalBudgetManager.Api.Repositories;
+using PersonalBudgetManager.Api.Repositories.Interfaces;
 
 namespace PersonalBudgetManager.Api.Controllers
 {
     [Route("[controller]")]
-    public class UserController(ILogger<UserController> logger) : Controller
+    public class UserController(ILogger<UserController> logger, IUnitOfWork unitOfWork) : Controller
     {
         private readonly ILogger<UserController> _logger = logger;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        private readonly Repository<User> _userRepo = unitOfWork.GetRepository<User>();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
