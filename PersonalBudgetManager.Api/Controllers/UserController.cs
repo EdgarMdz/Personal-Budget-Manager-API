6c08bf4b725960ee6581f45705e3fb8c343ce57e
Edgar Mendez <edgar.mdz@hotmail.com>
2025-02-03 23:50:37 -0600

    UserController now inherits from BaseController

--- PersonalBudgetManager.Api/Controllers/UserController.cs
+++ PersonalBudgetManager.Api/Controllers/UserController.cs
@@ -1,5 +1,3 @@
-using System.Security.Authentication;
-using Azure.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PersonalBudgetManager.Api.Models;
 using PersonalBudgetManager.Api.Services.Interfaces;
@@ -8,50 +6,44 @@ namespace PersonalBudgetManager.Api.Controllers
 {
     [Route("[controller]")]
     public class UserController(ILogger<UserController> logger, IUserService userService)
-        : Controller
+        : BaseController(logger)
     {
-        private readonly ILogger<UserController> _logger = logger;
         private readonly IUserService _userService = userService;
 
         [HttpPost]
         [Route("RegisterUser")]
         public async Task<IActionResult> CreateUser(UserDTO user, CancellationToken token)
         {
+            if (string.IsNullOrWhiteSpace(user.Password))
+                return BadRequest("password cannot be empty or whitespaces");
+
+            if (user.Password.Length < 8)
+                return BadRequest("the password must have a minimum of 8 characters");
+
+            bool hasNumbers = user.Password.Any(char.IsDigit);
+            bool hasCapitals = user.Password.Any(char.IsUpper);
+            bool hasSpecialCharacters = user.Password.Any(c => !char.IsLetterOrDigit(c));
+
+            if (!hasNumbers || !hasCapitals || !hasSpecialCharacters)
+                return BadRequest(
+                    "The password must contains at least one number, one capital letter and one spacial character"
+                );
+
             if (user.UserName.Trim() == string.Empty)
                 return BadRequest("Username cannot be empty or whitespaces");
 
             user.UserName = user.UserName.Trim();
-            try
+
+            async Task<IActionResult> action()
             {
                 if (await _userService.FindByName(user.UserName, token) != null)
                     return BadRequest("Choose a different user name");
 
-                if (string.IsNullOrWhiteSpace(user.Password))
-                    return BadRequest("password cannot be empty or whitespaces");
-
-                if (user.Password.Length < 8)
-                    return BadRequest("the password must have a minimum of 8 characters");
-
-                bool hasNumbers = user.Password.Any(char.IsDigit);
-                bool hasCapitals = user.Password.Any(char.IsUpper);
-                bool hasSpecialCharacters = user.Password.Any(c => !char.IsLetterOrDigit(c));
-
-                if (!hasNumbers || !hasCapitals || !hasSpecialCharacters)
-                    return BadRequest(
-                        "The password must contains at least one number, one capital letter and one spacial character"
-                    );
-
                 var newUser = await _userService.RegisterUser(user, token);
                 return Ok("User successfully registered. :)");
             }
-            catch (OperationCanceledException)
-            {
-                return StatusCode(449, "The request was canceled by the user");
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "An unexpected server error occurred. :(");
-            }
+
+            return await PerformActionSafely(action, user);
         }
 
         [HttpGet]
@@ -64,30 +56,13 @@ namespace PersonalBudgetManager.Api.Controllers
             if (string.IsNullOrWhiteSpace(user.Password))
                 return BadRequest("Provide a password");
 
-            try
+            async Task<IActionResult> action()
             {
                 user.UserName = user.UserName.Trim();
                 return Ok(await _userService.Login(user, token));
             }
-            catch (InvalidCredentialException e)
-            {
-                return BadRequest(e.Message);
-            }
-            catch (OperationCanceledException)
-            {
-                return StatusCode(449, "Operation canceled by the user");
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "An unexpected server error occurred. :(");
-            }
-        }
 
-        [HttpGet]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
+            return await PerformActionSafely(action, user);
         }
     }
 }
