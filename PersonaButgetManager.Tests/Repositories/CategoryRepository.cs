8fddd6ce2e045cec50be0cbcea1e3f2d02f0deae
Edgar Mendez <edgar.mdz@hotmail.com>
2025-03-12 20:20:04 -0600

    Add FindUserCategory test to validate retrieval of the last category for a user

--- PersonaButgetManager.Tests/Repositories/CategoryRepository.cs
+++ PersonaButgetManager.Tests/Repositories/CategoryRepository.cs
@@ -1,5 +1,4 @@
 using PersonaButgetManager.Tests.Common.Factories;
-using PersonalBudgetManager.Api.DataContext;
 using PersonalBudgetManager.Api.DataContext.Entities;
 
 namespace PersonaButgetManager.Tests.Repositories
@@ -36,6 +35,44 @@ namespace PersonaButgetManager.Tests.Repositories
             Assert.NotNull(result);
             Assert.Equal(categoryName, result.Name);
             Assert.Equal(userId, result.UserId);
+        }
+
+        [Fact]
+        public async Task FindUserCategory_WhenUserIdAmongMultipleUsersAndCategoryNameExist_ReturnsLastCategory()
+        {
+            // Arrange
+            await ResetDb(0);
+
+            var _categoryRepository = new PersonalBudgetManager.Api.Repositories.CategoryRepository(
+                _dbcontext,
+                DelegatestrategyFactory.NoOpStrategy()
+            );
+            var token = CancellationToken.None;
+            var categories = Enumerable
+                .Range(1, 100)
+                .Select(i => new Category()
+                {
+                    Id = i,
+                    Name = $"TestCategory {i}",
+                    UserId = i,
+                });
+            await _dbcontext.Categories.AddRangeAsync(categories);
+            await _dbcontext.SaveChangesAsync();
+
+            var category = _dbcontext.Categories.Last();
+
+            //Act
+            var result = await _categoryRepository.FindUserCategory(
+                category.UserId,
+                category.Name,
+                token
+            );
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(category.UserId, result.UserId);
+            Assert.Equal(category.Name, result.Name);
+            Assert.Equal(category.Id, result.Id);
         }
     }
 }
