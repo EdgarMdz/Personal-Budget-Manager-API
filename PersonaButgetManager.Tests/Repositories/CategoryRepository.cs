8dbda7b2f3e1215062680fb93ed8823c1de65aa9
Edgar Mendez <edgar.mdz@hotmail.com>
2025-03-12 20:30:54 -0600

    Add test for FindUserCategory to validate retrieval of a category among multiple users

--- PersonaButgetManager.Tests/Repositories/CategoryRepository.cs
+++ PersonaButgetManager.Tests/Repositories/CategoryRepository.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using PersonaButgetManager.Tests.Common.Factories;
 using PersonalBudgetManager.Api.DataContext.Entities;
 
@@ -38,6 +39,42 @@ namespace PersonaButgetManager.Tests.Repositories
                 category.Name,
                 token
             );
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(category.UserId, result.UserId);
+            Assert.Equal(category.Name, result.Name);
+            Assert.Equal(category.Id, result.Id);
+        }
+
+        [Fact]
+        public async Task FindUserCategory_WhenUserIdAmongMultipleUsersAndCategoryNameExist_ReturnsMiddleCategory()
+        {
+            //Arrange
+            await ResetDb(0);
+
+            var repo = new PersonalBudgetManager.Api.Repositories.CategoryRepository(
+                _dbcontext,
+                DelegatestrategyFactory.NoOpStrategy()
+            );
+
+            var categories = Enumerable
+                .Range(1, 100)
+                .Select(i => new Category()
+                {
+                    Id = i,
+                    Name = $"TestCategory {i}",
+                    UserId = i,
+                });
+
+            await _dbcontext.Categories.AddRangeAsync(categories);
+            await _dbcontext.SaveChangesAsync();
+
+            var category = _dbcontext.Categories.Skip(categories.Count() / 2).Take(1).First();
+            var token = CancellationToken.None;
+
+            //Act
+            var result = await repo.FindUserCategory(category.UserId, category.Name, token);
 
             //Assert
             Assert.NotNull(result);
