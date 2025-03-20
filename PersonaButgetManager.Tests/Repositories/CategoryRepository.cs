8ac637633e4580e13092fa1f0dc0d8fa8b871a19
Edgar Mendez <edgar.mdz@hotmail.com>
2025-03-19 19:59:10 -0600

    Add test for FindUserCategory to validate behavior when user ID does not exist

--- PersonaButgetManager.Tests/Repositories/CategoryRepository.cs
+++ PersonaButgetManager.Tests/Repositories/CategoryRepository.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
 using PersonaButgetManager.Tests.Common.Factories;
 using PersonalBudgetManager.Api.DataContext.Entities;
+using CategpryRepoAPI = PersonalBudgetManager.Api.Repositories;
 
 namespace PersonaButgetManager.Tests.Repositories
 {
@@ -15,7 +17,7 @@ namespace PersonaButgetManager.Tests.Repositories
             // Arrange
             await ResetDb(0);
 
-            var _categoryRepository = new PersonalBudgetManager.Api.Repositories.CategoryRepository(
+            var _categoryRepository = new CategpryRepoAPI.CategoryRepository(
                 _dbcontext,
                 DelegatestrategyFactory.NoOpStrategy()
             );
@@ -53,7 +55,7 @@ namespace PersonaButgetManager.Tests.Repositories
             //Arrange
             await ResetDb(0);
 
-            var repo = new PersonalBudgetManager.Api.Repositories.CategoryRepository(
+            var repo = new CategpryRepoAPI.CategoryRepository(
                 _dbcontext,
                 DelegatestrategyFactory.NoOpStrategy()
             );
@@ -81,6 +83,39 @@ namespace PersonaButgetManager.Tests.Repositories
             Assert.Equal(category.UserId, result.UserId);
             Assert.Equal(category.Name, result.Name);
             Assert.Equal(category.Id, result.Id);
+        }
+
+        [Fact]
+        public async Task FindUserCategory_WhenUserIdNotExistsAndCategoryNameExist_ReturnsNull()
+        {
+            //Arrange
+            await ResetDb(0);
+
+            var repo = new CategpryRepoAPI.CategoryRepository(
+                _dbcontext,
+                DelegatestrategyFactory.NoOpStrategy()
+            );
+            var categories = Enumerable
+                .Range(1, 100)
+                .Select(i => new Category()
+                {
+                    Id = i,
+                    Name = $"TestCategory {i}",
+                    UserId = i,
+                });
+
+            await _dbcontext.Categories.AddRangeAsync(categories);
+            await _dbcontext.SaveChangesAsync();
+
+            var category = new Category() { UserId = 500, Name = "TestCategory 50" };
+            var token = CancellationToken.None;
+
+            //Act
+
+            var result = await repo.FindUserCategory(category.UserId, category.Name, token);
+
+            //Assert
+            Assert.Null(result);
         }
     }
 }
