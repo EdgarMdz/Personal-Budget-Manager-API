63292f41816450b3c8851046f649bb116a1793b5
Edgar Mendez <edgar.mdz@hotmail.com>
2025-03-27 20:34:01 -0600

    add UnitOfWork test class for repository unit testing

---
+++ PersonaButgetManager.Tests/Repositories/UnitOfWork.cs
@@ -0,0 +1,7 @@
+using PersonalBudgetManager.Api.Common;
+using UOW = PersonalBudgetManager.Api.Repositories;
+
+namespace PersonaButgetManager.Tests.Repositories
+{
+    public class UnitOfWork : BaseTest { }
+}
